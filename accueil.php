bd0b104831b6af79bb0c8c3b4033734530e82bf6
Mekkaoui Abdessamad <110626317+MEKKAOUIABDESSAMAD@users.noreply.github.com>
2024-01-11 20:59:04 +0100

    Update accueil.php
    test1

--- accueil.php
+++ accueil.php
@@ -5,6 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php include 'inc/links.php' ?>
+  <!-- hello world-->
   <?php
     if (isset($_POST["submit"])) {
       $username = $_POST["name"];
